0bb0062025a5d37485b16e9c7987171dd0a0d38c
Sean 'Shaleh' Perry <shaleh@speakeasy.net>
2018-01-31 16:41:03 -0800

    Add HERE service tests.

--- service/tests/test_geocode.py
+++ service/tests/test_geocode.py
@@ -48,3 +48,54 @@ class GoogleGeocodeServiceTest(unittest.TestCase):
 
         with self.assertRaises(geocode.DataProcessingError):
             self.service.process_response('{"results": [{"geometry": {"location": {"lng": -122.0856086}}}] }')
+
+
+class HEREGeocodeServiceTest(unittest.TestCase):
+    def setUp(self):
+        self.service = geocode.HEREGeocodeService()
+
+    def test_prepare(self):
+        url = self.service.prepare({"APP_ID": "foo", "APP_CODE": "bar"},
+                                   "1+Way+There+Some+Place")
+        parsed = urlparse(url)
+        qs = parse_qs(parsed.query)
+        self.assertEqual(qs, {"searchtext": ["1+Way+There+Some+Place"],
+                              "app_id": ["foo"],
+                              "app_code": ["bar"]})
+        result_url, rest = url.split("?", 1)
+        self.assertEqual(result_url, self.service.url)
+
+    def test_process_response_ok(self):
+        json_file = os.path.join(os.path.dirname(__file__), "sample.here.json")
+        with open(json_file, "rb") as fp:
+            data = fp.read().decode()
+            result = self.service.process_response(data)
+            self.assertEqual(result, {"lat": '41.88449',
+                                      "lng": '-87.6387699'})
+
+        # Verify unexpected data is dropped
+        result = self.service.process_response('{"Response": {"View": [{"Result": [{"Location": {"NavigationPosition": [{"Latitude": 37.4224082, "Longitude": -122.0856086, "other": "extra"}]}}] }]}}')
+        self.assertEqual(result, {"lat": '37.4224082',
+                                  "lng": '-122.0856086'})
+
+    def test_process_response_fail(self):
+        with self.assertRaises(geocode.DataProcessingError):
+            self.service.process_response('{"Response": {} }')
+
+        with self.assertRaises(geocode.DataProcessingError):
+            self.service.process_response('{"Response": {"View": []} }')
+
+        with self.assertRaises(geocode.DataProcessingError):
+            self.service.process_response('{"Response": {"View": [{"Result": []}]} }')
+
+        with self.assertRaises(geocode.DataProcessingError):
+            self.service.process_response('{"Response": {"View": [{"Result": [{"Location": {}}]}]} }')
+
+        with self.assertRaises(geocode.DataProcessingError):
+            self.service.process_response('{"Response": {"View": [{"Result": [{"Location": {"NavigationPosition": []}}]}]} }')
+
+        with self.assertRaises(geocode.DataProcessingError):
+            self.service.process_response('{"Response": {"View": [{"Result": [{"Location": {"NavigationPosition": [{"Latitude": 123}]}}]}]} }')
+
+        with self.assertRaises(geocode.DataProcessingError):
+            self.service.process_response('{"Response": {"View": [{"Result": [{"Location": {"NavigationPosition": [{"Longitude": -678}]}}]}]} }')
